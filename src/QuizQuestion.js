ce698b6d0876a48b7cc9db119599274c39a328c1
jj2007 <jjayeshjadhav@yahoo.com>
2019-05-25 15:00:41 +0530

    Error handling

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,11 +4,14 @@ import QuizQuestionButton from './QuizQuestionButton';
 class QuizQuestion extends Component {
     constructor(props){
         super(props);
-
+        this.state = { incorrectAnswer: false } 
     }
     handleClick(buttonText) {
         if (buttonText == this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
+            this.setState({ incorrectAnswer: false })
+        } else {
+            this.setState({incorrectAnswer: true })
         }
     }
     render () {
@@ -24,6 +27,7 @@ class QuizQuestion extends Component {
                     })}
                 </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>Sorry , That Answer is false</p>: null}
             </main>
         );
     }
